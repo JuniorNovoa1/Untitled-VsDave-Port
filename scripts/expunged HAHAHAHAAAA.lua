f4e7b959ab602c48534287691b02bdd8a66e9617
Junior <87715171+JuniorNovoa1@users.noreply.github.com>
2023-11-13 21:45:33 -0500

    Update expunged HAHAHAHAAAA.lua

--- scripts/expunged HAHAHAHAAAA.lua
+++ scripts/expunged HAHAHAHAAAA.lua
@@ -2,11 +2,11 @@ function onCreate()
 	addHaxeLibrary("Process", 'sys.io')
 	addHaxeLibrary("File", 'sys.io')
 	addHaxeLibrary("Bytes", 'haxe.io')
+	addHaxeLibrary("Sys")
 end
 
 function onGameOver()
-	local expungedLines = 
-	{
+	local expungedLines = {
 		'[I FOUND YOU]', 
 		"[I CAN SEE YOU]", 
 		'[HAHAHHAHAHA]', 
@@ -17,13 +17,28 @@ function onGameOver()
 		"[I COULD DO THIS ALL DAY]",
 		"[DO THAT AGAIN, I LIKE WATCHING YOU FAIL]"
 	};
-	saveFile(currentModDirectory.."/HELLO.txt", expungedLines[getRandomInt(1, #expungedLines)], false)
 	if buildTarget == "windows" then
 		runHaxeCode([[
 			var fileContent = "]]..expungedLines[getRandomInt(1, #expungedLines)]..[[";
 			var path = Sys.getEnv("TEMP") + "/HELLO.txt";
 			File.saveContent(path, fileContent);
 			Sys.command("start " + path);
+		]])
+	end
+	if buildTarget == "mac" then
+		runHaxeCode([[
+			var fileContent = "]]..expungedLines[getRandomInt(1, #expungedLines)]..[[";
+			var path = Sys.getEnv("HOME") + "/HELLO.txt";
+			File.saveContent(path, fileContent);
+			Sys.command("open " + path);
+		]])
+	end
+	if buildTarget == "linux" then
+		runHaxeCode([[
+			var fileContent = "]]..expungedLines[getRandomInt(1, #expungedLines)]..[[";
+			var path = Sys.getEnv("HOME") + "/HELLO.txt";
+			File.saveContent(path, fileContent);
+			Sys.command("xdg-open " + path);
 		]])
 	end
 end
